dd0f96b1d444213cd27bb4df9453ec60f277ec2e
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-06-01 19:01:44 +0530

    feat: create a window object

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 #include "glad.h"
 #include <GLFW/glfw3.h>
+#include <print>
 
 int main()
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
-    return 0;
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window{glfwCreateWindow(800, 600, "PLXH", NULL, NULL)};
+  if (window == NULL) {
+    std::println("Failed to create GLFW window");
+    glfwTerminate();
+    return -1;
+  }
+  return 0;
 }
