11fc42041276c6475563f25197665afc32ec002f
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-06-14 19:21:38 +0530

    fix: buffer size and compilation errors

--- headers/shader.h
+++ headers/shader.h
@@ -1,4 +1,3 @@
-
 #ifndef PLXH_SHADER_H
 #define PLXH_SHADER_H
 
@@ -30,8 +29,8 @@ public:
             vertexShaderSource.close();
             fragmentShaderSource.close();
 
-            vertexCode{vshaderStream.str()};
-            fragmentCode{fshaderStream.str()};
+            vertexCode = vshaderStream.str();
+            fragmentCode = fshaderStream.str();
         } catch (std::istream::failure &e) {
             std::println("ERROR::SHADER::FAIL TO READ FILE: {}", e.what());
         }
@@ -40,17 +39,17 @@ public:
 
         GLuint vertex, fragment;
 
-        vertex{glCreateShader(GL_VERTEX_SHADER)};
+        vertex = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(vertex, 1, &vShaderCode,NULL);
         glCompileShader(vertex);
         checkCompileErrors(vertex, "VERTEX");
 
-        fragment{glCreateShader(GL_FRAGMENT_SHADER)};
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
         glShaderSource(fragment, 1, &fShaderCode,NULL);
         glCompileShader(fragment);
         checkCompileErrors(fragment, "FRAGMENT");
 
-        ID{glCreateProgram()};
+        ID = glCreateProgram();
         glAttachShader(ID, vertex);
         glAttachShader(ID, fragment);
         glLinkProgram(ID);
@@ -89,13 +88,13 @@ private:
         if (type != "PROGRAM") {
             glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
             if (!success) {
-                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+                glGetShaderInfoLog(shader, 512, NULL, infoLog);
                 std::println("ERROR: FAIL TO COMPILE SHADERS: {} \n infoLog: {}", type, infoLog);
             }
         } else {
             glGetProgramiv(shader, GL_LINK_STATUS, &success);
             if (!success) {
-                glGetProgramiv(shader, 1024,NULL, &infoLog);
+                glGetProgramInfoLog(shader, 512, NULL, infoLog);
                 std::println("ERROR: FAIL TO LINK SHADERS: {} \n infoLog: {}", type, infoLog);
             }
         }
