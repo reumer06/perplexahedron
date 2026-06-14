28074d08997689899888941b6169404d68c33536
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-06-14 19:08:53 +0530

    feat: add vertex, fragment shaders and program

--- shaders/shader.h
+++ shaders/shader.h
@@ -43,7 +43,21 @@ public:
         vertex{glCreateShader(GL_VERTEX_SHADER)};
         glShaderSource(vertex, 1, &vShaderCode,NULL);
         glCompileShader(vertex);
-        checkCompileError{vertex, "VERTEX"};
+        checkCompileErrors(vertex, "VERTEX");
+
+        fragment{glCreateShader(GL_FRAGMENT_SHADER)};
+        glShaderSource(fragment, 1, &fShaderCode,NULL);
+        glCompileShader(fragment);
+        checkCompileErrors(fragment, "FRAGMENT");
+
+        ID{glCreateProgram()};
+        glAttachShader(ID, vertex);
+        glAttachShader(ID, fragment);
+        glLinkProgram(ID);
+        checkCompileErrors(ID, "PROGRAM");
+
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
     }
 
     void use()
