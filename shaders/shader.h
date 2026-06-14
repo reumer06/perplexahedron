ad9f087ffbb01a28555bdea595b21a0093ae2ab0
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-06-14 18:54:37 +0530

    feat: add utility uniform functions

--- shaders/shader.h
+++ shaders/shader.h
@@ -31,10 +31,39 @@ public:
             fragmentShaderSource.close();
 
             vertexCode{vshaderStream.str()};
-            fragmentShaderSource{fshaderStream.str()};
+            fragmentCode{fshaderStream.str()};
         } catch (std::istream::failure &e) {
             std::println("ERROR::SHADER::FAIL TO READ FILE: {}", e.what());
         }
+        const GLchar *vShaderCode{vertexCode.c_str()};
+        const GLchar *fShaderCode{fragmentCode.c_str()};
+
+        GLuint vertex, fragment;
+
+        vertex{glCreateShader(GL_VERTEX_SHADER)};
+        glShaderSource(vertex, 1, &vShaderCode,NULL);
+        glCompileShader(vertex);
+        checkCompileError{vertex, "VERTEX"};
+    }
+
+    void use()
+    {
+        glUseProgram(ID);
+    }
+
+    void setBool(const std::string &name, bool value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int) value);
+    }
+
+    void setInt(const std::string &name, int value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+    }
+
+    void setFloat(const std::string &name, float value) const
+    {
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
     }
 };
 #endif
