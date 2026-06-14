7e66cdd493babc0ffaaa715d6a17c8d9b7c90289
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-06-14 18:29:00 +0530

    feat: include required headers

--- shader.h
+++ shader.h
@@ -2,4 +2,17 @@
 #ifndef PLXH_SHADER_H
 #define PLXH_SHADER_H
 
+#include <glad/glad.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <print>
+
+class Shader
+{
+public:
+    GLuint ID;
+
+    Shader(const char *vertexPath, const char *fragmentPath);
+};
 #endif //PLXH_SHADER_H
