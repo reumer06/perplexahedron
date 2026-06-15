c57017144fe6592e26e52f7130e27e327971cc23
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-06-15 23:23:06 +0530

    feat: create a texture & set wrapping and filtering parameters

--- src/main.cpp
+++ src/main.cpp
@@ -75,6 +75,16 @@ GLint main()
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+
     while (!glfwWindowShouldClose(window)) {
         process_input(window);
 
