24c762f741901485183316de825391db08dd628a
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-06-21 15:33:47 +0530

    feat: change the rotation axis

--- src/main.cpp
+++ src/main.cpp
@@ -149,9 +149,9 @@ int main()
         glm::mat4 model{glm::mat4(1.0f)};
         glm::mat4 view{glm::mat4(1.0f)};
         glm::mat4 projection{glm::mat4(1.0f)};
-        model = glm::rotate(model, (float) glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+        model = glm::rotate(model, (float) glfwGetTime() * glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        projection = glm::perspective(glm::radians(45.0f), (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 100.0f);
+        projection = glm::perspective(glm::radians(35.0f), (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 100.0f);
 
         GLuint modelLoc = glGetUniformLocation(shaders.ID, "model");
         GLuint viewLoc = glGetUniformLocation(shaders.ID, "view");
