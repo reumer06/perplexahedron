451c428187310acf2297a06797308f4edaaa75f1
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-06-21 15:11:11 +0530

    feat: add utility functions

--- include/shader.h
+++ include/shader.h
@@ -61,6 +61,11 @@ public:
     }
 
     // utility functions
+    void use() const
+    {
+        glUseProgram(ID);
+    }
+
     void setBool(const std::string &name, bool value) const
     {
         glUniform1i(glGetUniformLocation(ID, name.c_str()), (int) value);
