86a9abb5eb37b68b287329c314ac1e56c0922afb
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-06-21 19:56:16 +0530

    fix: add offset value

--- src/main.cpp
+++ src/main.cpp
@@ -172,7 +172,7 @@ int main()
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
 
-            float angle{20.0f * i};
+            float angle{20.0f * (i + 1)};
 
             model = glm::rotate(model, (float) glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             shaders.setMat4("model", model);
