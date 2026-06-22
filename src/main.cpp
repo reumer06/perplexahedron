4f308de494b52e124dd42db921967c33cfadae79
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-06-23 03:29:46 +0530

    feat: rotate around the world

--- src/main.cpp
+++ src/main.cpp
@@ -170,11 +170,9 @@ int main()
         glBindVertexArray(VAO);
         for (size_t i = 0; i < 10; ++i) {
             glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-
             float angle{20.0f * (i + 1)};
-
             model = glm::rotate(model, (float) glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            model = glm::translate(model, cubePositions[i]);
             shaders.setMat4("model", model);
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
