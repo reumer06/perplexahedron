fe8551d97f1809ade8c3aec3ae3f140fc3e7d4ac
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-06-30 09:45:35 +0530

    feat: add constructors

--- include/camera.h
+++ include/camera.h
@@ -32,8 +32,30 @@ public:
     float Yaw;
     float Pitch;
     float MovementSpeed;
-    float MousSensitivity;
+    float MouseSensitivity;
     float Zoom;
+
+    // vector constructor
+    Camera(glm::
+           vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW,
+           float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED),
+                                  MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    {
+        Position = position;
+        WorldUp = up;
+        Yaw = yaw;
+        Pitch = pitch;
+    }
+
+    // scalar constructor
+    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(
+            glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    {
+        Position = glm::vec3(posX, posY, posZ);
+        WorldUp = glm::vec3(upX, upY, upZ);
+        Yaw = yaw;
+        Pitch = pitch;
+    }
 };
 
 
