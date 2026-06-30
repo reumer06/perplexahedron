aa3ea6d9f2e550c00c741e30592786047ba7fce4
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-06-30 09:35:30 +0530

    feat: define camera attributes and euler angles

--- include/camera.h
+++ include/camera.h
@@ -19,5 +19,22 @@ const float SPEED{2.5f};
 const float SENSITIVITY{0.1f};
 const float ZOOM{45.0f};
 
+class Camera
+{
+public:
+    glm::vec3 Front;
+    glm::vec3 Position;
+    glm::vec3 Right;
+    glm::vec3 Up;
+    glm::vec3 WorldUp;
+
+    // euler angles
+    float Yaw;
+    float Pitch;
+    float MovementSpeed;
+    float MousSensitivity;
+    float Zoom;
+};
+
 
 #endif
