0eefdb9203c2ede2da06db57ee6361296c580e5b
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-06-30 10:08:32 +0530

    feat: add update camera Vectors from euler angles

--- include/camera.h
+++ include/camera.h
@@ -77,6 +77,19 @@ public:
                 Pitch - -89.0f;
         }
     }
+
+private:
+    void updateCameraVectors()
+    {
+        glm::vec3 front;
+        front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+        front.y = sin(glm::radians(Pitch));
+        front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+        Front = glm::normalize(front)
+
+        Right = glm::normalize(glm::cross(Front, WorldUp));
+        Up = glm::normalize(glm::cross(Right, Front));
+    }
 };
 
 
