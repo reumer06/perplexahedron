0b7e301de7de5af714da46ffe283dcf5ac6b4732
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-08-04 15:49:49 +0530

    fix: vertexAttribPointer size

--- src/main.cpp
+++ src/main.cpp
@@ -131,7 +131,7 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) 0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
     glBindVertexArray(lightVAO);
